ff2c9ac735b94147c1f69c1c9c1f300a81f3bb61
Dheepakkamal <mail@dheepak.com>
2023-11-03 18:07:42 +0530

    Added gitignore

---
+++ src/main/java/com/dheepak/learning/java/random/SampleHashMap.java
@@ -0,0 +1,12 @@
+package com.dheepak.learning.java.random;
+
+import com.dheepak.learning.java.random.Pojos.Employee;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SampleHashMap {
+    public static void main(String[] args) {
+        Map<Employee, Employee> employeeMap = new HashMap<>();
+    }
+}
