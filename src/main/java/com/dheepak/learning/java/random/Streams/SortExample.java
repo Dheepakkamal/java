a3666b05d48be85729f43e0f05cb3b753f0c607d
Dheepakkamal <mail@dheepak.com>
2023-11-27 21:31:04 +0530

    Update SortExample.java

--- src/main/java/com/dheepak/learning/java/random/Streams/SortExample.java
+++ src/main/java/com/dheepak/learning/java/random/Streams/SortExample.java
@@ -1,27 +1,26 @@
-//package com.dheepak.learning.java.random.Streams;
-//
-//import java.util.ArrayList;
-//import java.util.Collections;
-//import java.util.Comparator;
-//import java.util.List;
-//
-//public class SortExample {
-//
-//    public static List<String> getSongsList() {
-//        List<String> songs = new ArrayList<>();
-//        songs.add("somerault");
-//        songs.add("cassidy");
-//        songs.add("havana");
-//        songs.add("$10");
-//        songs.add("50 ways");
-//        songs.add("Cassidy");
-//        return songs;
-//    }
-//    public static void main(String[] args) {
-//        List<String> songs = getSongsList();
-//        Collections.sort(songs);
-//        Collections.sort(songs, new Comparator);
-//        System.out.println(songs);
-//        songs.stream().
-//    }
-//}
+package com.dheepak.learning.java.random.Streams;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class SortExample {
+
+    public static List<String> getSongsList() {
+        List<String> songs = new ArrayList<>();
+        songs.add("somerault");
+        songs.add("cassidy");
+        songs.add("havana");
+        songs.add("$10");
+        songs.add("50 ways");
+        songs.add("Cassidy");
+        return songs;
+    }
+    public static void main(String[] args) {
+        List<String> songs = getSongsList();
+        Collections.sort(songs);
+        Collections.sort(songs, String::compareTo);
+        System.out.println(songs);
+    }
+}
